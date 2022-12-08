52e39c8fc96932cf2dc33bc89d1d597ede9cc6f1
alxvngn <alexviean@gmail.com>
2022-12-08 18:07:02 -0500

    Update installation script

--- install.sh
+++ install.sh
@@ -3,7 +3,15 @@
 # If this is run from SSH, your connection will disconnect when this script is done.
 
 git clone https://github.com/alxvngn/pi-as-keyboard
-cd pi-as-keyboard
-./setup-hid-modules.sh
-./enableHIDRCLocal.sh
+
+if ! grep 'dtoverlay=dwc2' /boot/config; then
+  echo "dtoverlay=dwc2" | sudo tee -a /boot/config.txt
+fi
+
+if ! grep dwc2 /etc/modules; then
+  echo "dwc2" | sudo tee -a /etc/modules
+fi
+
+./pi-as-keyboard/enableHIDRCLocal.sh
+
 poweroff
--- setup-hid-modules.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-echo "dtoverlay=dwc2" | sudo tee -a /boot/config.txt
-echo "dwc2" | sudo tee -a /etc/modules
